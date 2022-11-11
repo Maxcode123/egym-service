0021e57234981f407ae47153a94783dd6e9bb2bc
Maximos Nikiforakis <nikiforos@live.co.uk>
2022-11-11 23:30:28 +0200

    middleware

--- api/endpoints/egym.py
+++ api/endpoints/egym.py
@@ -1,5 +1,5 @@
-from fastapi import FastAPI
-from fastapi import HTTPException
+from fastapi import FastAPI, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 
 from api.business_logic.DTOs import TrainerProfile, TraineeProfile
 from api.providers.factory import ProvidersFactory
@@ -7,6 +7,15 @@ from api.business_logic.exc import DbConnectionError, SQLError
 
 
 app = FastAPI(docs_url="/swagger")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 @app.post("/write_trainer")
 def write_trainer(user: TrainerProfile):
